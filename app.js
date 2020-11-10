af302ee9adee0500ca5c24f9150519c38b3be9cd
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-11-09 19:27:28 -0500

    [fixed set timout issue]

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("testing")
+
 const movementDisplay = document.querySelector ('#movement')
 const game = document.querySelector('#game')
 
@@ -19,47 +19,19 @@ const ctx = game.getContext('2d')
           this.color = color
           this.width = width
           this.height = height
-          this.direction = direction //snake can now move automatically
-          //this.alive = true chose to use relocation instead of making the block disappear
-    
+          this.direction = direction //snake can now move automatically    
 }
 render(){
     ctx.fillStyle = this.color
     ctx.fillRect(this.x, this.y, this.width, this.height)
 }
-// class block {
-//     constructor(x, y, color, width, height){
-//         this.x = x
-//         this.y = y
-//         this.color = color
-//         this.width = width
-//         this.height = height
-  
-// }
+
 }
 
 const snake = new snakeBlock(200, 200, '#55dac6', 10, 10, 'left')
 const block1 = new snakeBlock(30, 30, 'blue', 10, 10, 'null')
 let score = 0;
-//onst block12 = new snakeBlock(300, 300, 'blue', 10, 10, 'null')
 
-// class Block {
-//     constructor(x, y, color, width, height, direction){
-    //         this.x = x
-    //         this.y = y
-    //         this.color = color
-    //         this.width = widthwwwww
-    //         this.height = height
-    //         this.direction = direction
-    
-    
-    //   document.getElementById('status').addEventListener('click', function(){
-        //       block.render()
-        //       
-        
-        //   })
-        
-        //   const firstBlock = new Block(100,100, '#55dac6', 10, 10, 'right')
         
     //this function allows me to use buttoons on my key board
     document.addEventListener('keyup', function(evt){
@@ -77,7 +49,9 @@ let score = 0;
             
         }
         
-        
+        console.log('snake.x', snake.x)
+
+        console.log('snake.y', snake.y)
         
         movementDisplay.textContent = `X:${snake.x}, Y${snake.y}`
     })
@@ -116,15 +90,13 @@ let score = 0;
         snake.render()
         block1.render()
 
-        // if(block1.alive){
-            //     block1.render()
-            // }
             winner()
             collisionWall()
             eatingBlock()
-            //collisionBlock()
+           
         }
-        setInterval(rePaint, 1000/20 ) 
+     const intervalID = setInterval(rePaint, 1000/20 ) 
+     
         
     //start with collision detection between snake and block1
     function eatingBlock() {
@@ -133,9 +105,9 @@ let score = 0;
             && snake.y < block1.y + block1.height
             && snake.y + snake.height > block1.y
             ){
-                // block1.alive = false
+                //
                 // Need to render the block in a new position on the board
-                //this relocates the blocks and add to the snakeswidth
+                //this relocates the blocks and add to the snakes width and snake height
                 relocateBlock();
                 score += 1;
                 document.getElementById('score').innerText = 'Score ' + score
@@ -145,86 +117,54 @@ let score = 0;
             }
         }
     //function to declare you won the game
+    //It also allways you to win after the 10 bite
     function winner(){
         if(score === 10){
+            clearCanvas()
             gameWin()
             
-            //resetCanvas()
+        
         }
     }
-    //Reset the game
+    //Reset the game with click event
     document.getElementById("reset").addEventListener("click",function(){
         resetCanvas()
     });
-
-    //Start the game
-    // document.getElementById("status").addEventListener("click", function(){
-    //     
-    // })
-                
-                
-                //if (snake.x + snake.width > block1.x){
-                    //      console.log('hit 2')
-                    //  }
-                    //     snake.x < block1.x + block1.width &&
-    //     snake.y + snake.height > block1.y &&
-    //     snake.y < block1.y + block1.height ) { 
-
-    //     block1.alive = false
-        
-        //block1 = new Block(newX, newY, 10, 10, 'blue');
-    
-
-
-// function randomPosition(){
-    
-
-    
-//     Block1.render()
-//     // Block2.render()
-//     // Block3.render()
-//     // Block4.render()
-//     // Block5.render()
-//     // Block6.render()
-
-// }
-// setInterval(randomPosition, 1000/20)
-
 
 
 // collision detection with game canvas and ending the game
 function collisionWall(){
     if(snake.y > game.height){
-        
-        //resetCanvas()
         clearCanvas()
         gameOverMessage()
+    
         
     }else if(snake.x < 0){
         
-        //resetCanvas()
         clearCanvas()
         gameOverMessage()
+
         
     }else if(snake.x > game.width){
         
-        //resetCanvas()
         clearCanvas()
         gameOverMessage()
         
     }else if(snake.y <0){
         
-        //resetCanvas()
         clearCanvas()
         gameOverMessage()
         
 }
 }
+
+//Clears my blocks off the canvas and stops animation
 function clearCanvas(){
     ctx.clearRect(0,0, game.width, game.height)
+    clearInterval(intervalID);
 }
 
-// will tell user they lost and to restart the page for now
+//will tell user they lost
 
 function gameOverMessage() {
     
@@ -235,11 +175,13 @@ function gameOverMessage() {
     
     ctx.fillText('You lost the game try again',  game.width/2 , game.height/2);
 }
+
+//This allows to restart the everything
 function resetCanvas() {
     window.location.reload();
 }
 
-//function to display you win
+//function to display you win message 
 
 function gameWin() {
     
@@ -251,36 +193,5 @@ function gameWin() {
     ctx.fillText('You Win',  game.width/2 , game.height/2);
 }
 
-
-  
-
-// const newBlock = new snakeBlock(10, 10, 'blue', 10, 10, 'null')
-
-// function randomBlocks() {
-//     let position = x, y;
-//     for (let i = 0; i < 10; i++) {
-//         position += (Math.random() * 16 | 0;
-//     }
-//     return position;
-// }
-
-//eat random blocks like packman
-
-
-
-
-
-
-//you win when you eat 8 or 10 blocks still deciding varible that keeps track of how many blocks youve eaten
-
-//being able to eat a block and win is a requirement 
-
-//push function for multiblock snake create an empty array and push new blocks into it
-
-
-// function to make the block move automatically using unshift
-
-//using unshift 
-//function snakeMovement()
 
 
